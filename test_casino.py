0e7ac41d6e7097af5ab670f666fac01270641dd4
David Ringgenberg <david.ringgenberg@ringgi.net>
2020-06-19 12:03:47 +0200

    refactor: Update casino test cases

--- test_casino.py
+++ test_casino.py
@@ -1,29 +1,20 @@
 import os
 import unittest
 import json
-from flask_sqlalchemy import SQLAlchemy
 
-from app import create_app
-from models import Jeton
-
+from app import app
 
 class CasinoTestCase(unittest.TestCase):
     """This class represents the casino test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
+        self.app = app
+        self.client = app.test_client
 
         self.new_jeton = {
-            'userId': 'lkajsfu483ijz6932kj12fasjl4',
+            'user_id': 'lkajsfu483ijz6932kj12fasjl4',
             'jeton_amount': 2000
         }
-
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
     
     def tearDown(self):
         """Executed after reach test"""
@@ -31,25 +22,29 @@ class CasinoTestCase(unittest.TestCase):
     
     # Correct test create_jeton() request
     def test_create_jeton(self):
-        res = self.client().post('/jeton', json=self.new_jeton)
+        res = self.client().post('/jetons', json=self.new_jeton)
         self.assertEqual(res.status_code, 200)
     
-    # Correct test get_jeton(lkajsfu483ijz6932kj12fasjl4) endpoint for retrieving a jeton
+    # Correct test get_jeton(asdf1234fdsa) endpoint for retrieving a jeton
     def test_get_jeton(self):
         res = self.client().get('/jetons/asdf1234fdsa')
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.json['jeton_amount'], 204820)
         self.assertEqual(res.json['user_id'], 'asdf1234fdsa')
+
+    # Error test create_jeton() request
+    def test_create_jeton_already_exists(self):
+        res = self.client().post('/jetons', json=self.new_jeton)
+        self.assertEqual(res.status_code, 404)
 
     # Error test get_jeton() request
     def test_get_jeton_not_found(self):
-        res = self.client().get('/jetons/thisoneisnothere')
+        res = self.client().get('/jetons/qwertzuiop234567890')
         self.assertEqual(res.status_code, 404)
 
     # Error test create_jeton() internal server error
     def test_create_jeton_internal_server_error(self):
-        res = self.client().post('/jetons', json={'question':'Hello'})
-        self.assertEqual(res.status_code, 500)
+        res = self.client().post('/jetons/jetons', json={'question':'Hello'})
+        self.assertEqual(res.status_code, 405)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
