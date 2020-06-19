dd529cf7fc8b6e3aabbbfc39228fa36a317f9607
David Ringgenberg <david.ringgenberg@ringgi.net>
2020-06-19 12:02:46 +0200

    feat: POST Jeton request to INI file
    interfaces.py:
    post_jeton function setup
    
    inifile_jeton_context.py:
    Overwrite post_jeton function from IJetonContext
    
    casino_singleton.py:
    Return post_jeton from IniFileJetonContext
    
    app.py:
    Request is checked if all needed parameters are given with it.
    New Jeton get written in INI file and returns the newly written json
    Return json with new jeton

--- inifile_jeton_context.py
+++ inifile_jeton_context.py
@@ -13,9 +13,29 @@ database.read(database_ini)
 
 class IniFileJetonContext(IJetonContext):
     
-    def get_jeton(user_id) -> Jeton:
+    def get_jeton(user_id: str) -> Jeton:
+        database.read(database_ini)
         jeton_amount = int(database[user_id]['jeton_amount'])
         return Jeton(user_id, jeton_amount)
     
     def get_jeton_factor() -> float:
+        config.read(config_ini)
         return float(config['jeton']['factor'])
+
+    def post_jeton(user_id: str, jeton_amount: int) -> Jeton:
+        database.read(database_ini)
+
+        database.add_section(user_id)
+        database.set(
+            user_id, 
+            "jeton_amount", str(jeton_amount)
+        )
+
+        cfgfile = open(database_ini, 'w')
+        database.write(cfgfile)
+        cfgfile.close()
+
+        database.read(database_ini)
+        new_jeton_amount = int(database[user_id]['jeton_amount'])
+
+        return Jeton(user_id, new_jeton_amount)
