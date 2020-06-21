04c5e93117f32e6a29bd2bd83f96b925d1799495
JonathanCarona <jonathan.carona@competec.ch>
2020-06-21 12:32:46 -0700

    Moved tests to tests folder

--- tests/PaymentStrategyTest.py
+++ tests/PaymentStrategyTest.py
@@ -6,15 +6,15 @@ class PaymentStrategyTestCase(unittest.TestCase):
     """This class represents the payment strategy test case"""
 
     def setUp(self):
-        self.context = PaymentContext(TwintPayment)
+        self.context = PaymentContext()
 
     def test_creditcard_strategy(self):
         self.context.strategy = CreditcardPayment()
-        self.assertTrue(self.context.payWithMethod())
+        self.assertTrue(self.context.payWithMethod(1000.0))
 
     def test_anotherPayment_strategy(self):
         self.context.strategy = TwintPayment()
-        self.assertFalse(self.context.payWithMethod())
+        self.assertFalse(self.context.payWithMethod(1000.0))
 
 
 
