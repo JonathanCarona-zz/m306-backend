2fb72fb2990f3ccffbd28ed23317fa28dbc3c39a
JonathanCarona <jonathan.carona@competec.ch>
2020-06-23 02:35:09 -0700

    bug fix

--- flaskr/services/SlotMachineService.py
+++ flaskr/services/SlotMachineService.py
@@ -19,10 +19,10 @@ class SlotMachineService:
         if (slotmachine_result[0] == 0 and slotmachine_result[1] == 0 and slotmachine_result[2] == 0):
             IniFileJetonContext.set_player_jeton(user_id, bet * 7 + IniFileJetonContext.get_jeton(user_id).jeton_amount)
             profit = bet * 7
-        elif (slotmachine_result[1] == 1 and slotmachine_result[1] == 1 and slotmachine_result[1] == 1):
+        elif (slotmachine_result[0] == 1 and slotmachine_result[1] == 1 and slotmachine_result[2] == 1):
             IniFileJetonContext.set_player_jeton(user_id, bet * 5 + IniFileJetonContext.get_jeton(user_id).jeton_amount)
             profit = bet * 5
-        elif (slotmachine_result[2] == 2 and slotmachine_result[2] == 2 and slotmachine_result[2] == 2):
+        elif (slotmachine_result[0] == 2 and slotmachine_result[1] == 2 and slotmachine_result[2] == 2):
             IniFileJetonContext.set_player_jeton(user_id, bet * 2 + IniFileJetonContext.get_jeton(user_id).jeton_amount)
             profit = bet * 2
 
